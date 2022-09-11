c0a35667a7fa5618ea5f8e0d7220cf5d64863ba2
Rohit Gupta <lucidrohit@gmail.com>
2022-09-11 09:23:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+import { getData } from "./utils/weatherApi/weatherApi";
+import { BallTriangle } from "react-loader-spinner";
 
 function App() {
+  const cityRef = useRef();
+  const [loading, setLoading] = useState(true);
+  const [weatherData, setWeatherData] = useState({});
+
+  const handleClick = async () => {
+    setLoading(true);
+    const data = await getData(cityRef.current.value);
+    setWeatherData(data);
+    setLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading ? (
+        <header className="App-header">
+          <img
+            src={weatherData.icon}
+            alt={weatherData.text}
+            style={{ width: "200px" }}
+          />
+          <p>{weatherData.text}</p>
+          <h3>Temp:{weatherData.temperature} Deg Celcius</h3>
+          <p>
+            {weatherData.name},{weatherData.region},{weatherData.country}
+          </p>
+        </header>
+      ) : (
+        <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#4fa94d"
+          ariaLabel="ball-triangle-loading"
+          wrapperClass={{}}
+          wrapperStyle=""
+          visible={true}
+        />
+      )}
+      <input
+        type={"text"}
+        ref={cityRef}
+        style={{
+          height: "40px",
+          width: "300px",
+          borderRadius: "10px",
+          padding: "0.5rem 1rem",
+          fontSize: "25px",
+          fontWeight:"700",
+          outline: "none",
+          textAlign: "center",
+          display: "block",
+          marginTop: "3rem",
+        }}
+        placeholder="City..."
+      />
+      <input
+        type={"button"}
+        onClick={handleClick}
+        value="Search"
+        style={{
+          height: "50px",
+          width: "200px",
+          borderRadius: "10px",
+          padding: "0.5rem 1rem",
+          fontSize: "20px",
+          margin: "2rem",
+          textAlign: "center",
+          display: "block",
+          cursor: "pointer",
+        }}
+      />
     </div>
   );
 }
